f703da55b714373c96b572913d861d1f400b3002
markthitrin <thitrinwork@gmail.com>
2024-01-15 17:27:29 +0700

    Mo algorithm

--- MoAlgorithm.cpp
+++ MoAlgorithm.cpp
@@ -87,7 +87,34 @@ ll get_mode(node* root) {
 }
 
 std::vector<ll> MO_algorithm(std::vector<ll>& a,std::vector<query> Q) {
-   return std::vector<ll>();
+    std::sort(Q.begin(),Q.end(),[](query qu1,query qu2) {
+        if(qu1.left / sqrt_N != qu2.left / sqrt_N) return qu1.left < qu2.left;
+        return qu1.right < qu2.right;
+    });
+    ll cur_left = a.size();
+    ll cur_right = -1;
+    std::vector<ll> ans(Q.size(),0);
+    node* root = new node();
+    for(int q = 0;q < Q.size();q++) {
+        while(cur_left < Q[q].left) {
+            subtract(root,a[cur_left]);
+            ++cur_left;
+        }
+        while(cur_left > Q[q].left) {
+            --cur_left;
+            add(root,a[cur_left]);
+        }
+        while(cur_right > Q[q].right) {
+            subtract(root,a[cur_right]);
+            --cur_right;
+        }
+        while(cur_right < Q[q].right) {
+            ++cur_right;
+            add(root,a[cur_right]);
+        }
+        ans[Q[q].query_number] = get_mode(root);
+    }
+    return ans;
 }
 
 ll get_mode_brute_force(std::vector<ll>& a,ll left,ll right) {
@@ -114,7 +141,25 @@ ll get_rand_value() {
 }
 
 bool run_test() {
-    return true;
+    N = (ull)get_rand_value() % 20000 + 20000;
+    ll M = (ull)get_rand_value() % 20000 + 20000;
+    sqrt_N = std::sqrt(N);
+    std::vector<ll> a(N);
+    std::vector<query> qu;
+    for(int q = 0;q < a.size();q++) {
+        a[q] = get_rand_value() % 1000;
+    }
+    for(int q = 0;q < M;q++) {
+        qu.push_back(query());
+        qu[q].left = (ull)get_rand_value() % N;
+        qu[q].right = (ull)get_rand_value() % N;
+        qu[q].query_number = q;
+    }
+    std::vector<ll> ans(qu.size());
+    for(int q =0 ;q <qu.size();q++) {
+        ans[q] = get_mode_brute_force(a,qu[q].left,qu[q].right); 
+    }
+    return ans == MO_algorithm(a, qu);
 }
 
 int main() {
