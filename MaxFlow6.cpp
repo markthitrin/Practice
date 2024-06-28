9e1a8cab3b4aa8b7b13383e8fa2379ca2347b60f
markthitrin <thitrinwork@gmail.com>
2024-06-28 21:26:33 +0700

    lower bound maxflow

--- MaxFlow6.cpp
+++ MaxFlow6.cpp
@@ -3,61 +3,62 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#define vi std::vector<int>
+#define sz(v) v.size()
+#define rep(i,a,b) for(int i = a;i < b;i++)
+using namespace std;
 
 using ll = long long;
 
 struct PushRelabel {
-    struct Edge {
-        int dest, back;
-        ll f, c;
-    };
-    std::vector<std::vector<Edge>> g;
-    std::vector<ll> ec;
-    std::vector<Edge*> cur;
-    std::vector<std::vector<int>> hs; std::vector<int> H;
-    PushRelabel(int n) : g(n), ec(n), cur(n), hs(2 * n), H(n) {}
-
-    void addEdge(int s,int t, ll cap,ll rcap = 0) {
-        if(s == t) return;
-        Edge e;
-        e.dest = t,e.back = g[t].size(),e.f = 0,e.c = cap;
-        g[s].push_back(e);
-        e.dest = s,e.back = g[s].size() - 1,e.f = 0,e.c = rcap;
-        g[t].push_back(e);
-    }
-
-    void addFlow(Edge& e, ll f) {
-        Edge& back = g[e.dest][e.back];
-        if(!ec[e.dest] && f) hs[H[e.dest]].push_back(e.dest);
-        e.f += f,e.c -= f;ec[e.dest] += f;
-        back.f -= f;back.c += f; ec[back.dest] -= f;
-    }
-    ll calc(int s,int t) {
-        int v = g.size();H[s] = v;ec[t] = 1;
-        std::vector<int> co(2*v);co[0] = v - 1;
-        for(int i = 0;i < v;i++) cur[i] = g[i].data();
-        for(Edge& e : g[s]) addFlow(e,e.c);
-
-        for(int hi = 0;;) {
-            while(hs[hi].empty()) if(!hi--) return -ec[s];
-            int u = hs[hi].back(); hs[hi].pop_back();
-            while(ec[u] > 0) // discharge u
-                if(cur[u] == g[u].data() + g[u].size()) {
-                    H[u] = 1e9;
-                    for(Edge& e : g[u]) if(e.c && H[u] > H[e.dest] + 1)
-                        H[u] = H[e.dest] + 1,cur[u] = &e;
-                    if(++co[H[u]], !--co[hi] && hi < v) 
-                        for(int i = 0;i < v;i++) if(hi < H[i] && H[i] < v)
-                            --co[H[i]], H[u] = v + 1;
-                    hi = H[u];
-                }else if(cur[u]->c && H[u] == H[cur[u]->dest] + 1)
-                    addFlow(*cur[u], std::min(ec[u],cur[u]->c));
-                else ++cur[u];
-        }
-    }
-    bool leftOfMinCut (int a) {return H[a] >= g.size();}
+struct Edge {
+int dest, back;
+ll f, c;
 };
+vector<vector<Edge>> g;
+vector<ll> ec;
+vector<Edge*> cur;
+vector<vi> hs; vi H;
+PushRelabel(int n) : g(n), ec(n), cur(n), hs(2*n), H(n) {}
+void addEdge(int s, int t, ll cap, ll rcap=0) {
+if (s == t) return;
+Edge e;
+e.dest = t;e.back = sz(g[t]);e.f = 0;e.c = cap;
+g[s].push_back(e);
+e.dest = s;e.back = sz(g[s]) - 1;e.f = 0;e.c = rcap;
+g[t].push_back(e);
+}
+void addFlow(Edge& e, ll f) {
+Edge &back = g[e.dest][e.back];
+if (!ec[e.dest] && f) hs[H[e.dest]].push_back(e.dest);
+e.f += f; e.c -= f; ec[e.dest] += f;
+back.f -= f; back.c += f; ec[back.dest] -= f;
+}
+ll calc(int s, int t) {
+int v = sz(g); H[s] = v; ec[t] = 1;
+vi co(2*v); co[0] = v-1;
+rep(i,0,v) cur[i] = g[i].data();
+for (Edge& e : g[s]) addFlow(e, e.c);
+for (int hi = 0;;) {
+while (hs[hi].empty()) if (!hi--) return -ec[s];
+int u = hs[hi].back(); hs[hi].pop_back();
+while (ec[u] > 0) // discharge u
+if (cur[u] == g[u].data() + sz(g[u])) {
+H[u] = 1e9;
+for (Edge& e : g[u]) if (e.c && H[u] > H[e.dest]+1)
+H[u] = H[e.dest]+1, cur[u] = &e;
+if (++co[H[u]], !--co[hi] && hi < v)
+rep(i,0,v) if (hi < H[i] && H[i] < v)
+--co[H[i]], H[i] = v + 1;
+hi = H[u];
+} else if (cur[u]->c && H[u] == H[cur[u]->dest]+1)
+addFlow(*cur[u], min(ec[u], cur[u]->c));
+else ++cur[u];
+}
+}
+bool leftOfMinCut(int a) { return H[a] >= sz(g); }
+};
+
 
 int main() {
     int N,M;
@@ -113,27 +114,23 @@ int main() {
         std::cout << -1 << std::endl;
         return 0;
     }
-    ll default_flow = in[N + M + 1];
+
+    ll default_flow = 0; 
     for(int q = 0;q < PR.g[N + M + 1].size();q++) {
-        PushRelabel::Edge& e = PR.g[N + M + 1][q];
-        if(e.dest > N + M + 1) 
-            continue;
-        if(e.dest == 0) 
-            continue;
-        default_flow += -e.f;
+        if(PR.g[N + M + 1][q].dest == 0) {
+            default_flow = PR.g[N + M + 1][q].f;
+            break;
+        }
     }
     
-    PushRelabel maxPR(N + M + 2);
-    for(int q = 0;q < N + M + 2;q++) {
+    PushRelabel maxPR(N + M + 4);
+    for(int q = 0;q < N + M + 4;q++) {
         for(int w = 0;w < PR.g[q].size();w++) {
             PushRelabel::Edge& e = PR.g[q][w];
-            if(e.f + e.c == 0) {
-                continue;
-            }
-            if(e.dest > N + M + 1) {
-                continue;
-            }
             if(q == N + M + 1 && e.dest == 0) {
+                continue;
+            }
+            if(q == 0 && e.dest == N + M + 1) {
                 continue;
             }
             maxPR.addEdge(q,e.dest,e.c);
@@ -142,21 +139,22 @@ int main() {
     ll max_flow_ans = maxPR.calc(0,N + M + 1);
     max_flow_ans += default_flow;
 
-    PushRelabel minPR(N + M + 2);
-    for(int q = 0;q < N + M + 2;q++) {
+    PushRelabel minPR(N + M + 4);
+    for(int q = 0;q < N + M + 4;q++) {
         for(int w = 0;w < PR.g[q].size();w++) {
             PushRelabel::Edge& e = PR.g[q][w];
-            if(e.dest >= N + M + 1) {
+            if(q == 0 && e.dest == N + M + 1) {
                 continue;
             }
-            if(e.f + e.c == 0) {
-                minPR.addEdge(q,e.dest,e.c); 
+            if(q == N + M + 1 && e.dest == 0) {
+                continue;
             }
+            minPR.addEdge(q,e.dest,e.c);
         }
     }
     ll min_flow_ans = default_flow;
     min_flow_ans -= minPR.calc(N + M + 1,0);
 
-    std::cout << min_flow_ans << "\n" << max_flow_ans << std::endl;
+    std::cout << min_flow_ans << std::endl << max_flow_ans << std::endl;
     return 0;
 }
